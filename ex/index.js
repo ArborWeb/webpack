cb5e4e9ee9c1e173e8f849d3563a2c44fd5be900
Arbor Web <guilherme@arborweb.com.br>
2017-08-01 11:00:42 -0300

    finalizada aula 13 - Interpretando o Operador Spread (Plugin Babel)

--- ex/index.js
+++ ex/index.js
@@ -1,4 +1,15 @@
-import Pessoa from './pessoa'
+const produto = {
+	nome: 'Caneta BIC Preta',
+	preco: 1.90,
+	desconto: 0.05
+}
 
-const pessoa = new Pessoa('Guilherme')
-console.log(pessoa.toString())
+function clone(objeto) {
+	return { ...objeto }
+}
+
+const novoProduto = clone(produto)
+novoProduto.nome = 'Caneta BIC Azul'
+novoProduto.preco = 2
+
+console.log(produto, novoProduto)
